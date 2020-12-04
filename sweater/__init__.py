be45fe10e5c8e311b6dc360844f950dcd2719609
memberember <64272753+memberember@users.noreply.github.com>
2020-12-04 20:12:26 +0500

    Раскидал по архитектуре, добавил авторизацию, регистрацию и логаут - Дмитрий Тен 04.12.2020

---
+++ sweater/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+# подключаем фласк и sqlalchemy для работы с бд
+app = Flask(__name__)
+app.secret_key = 'some secret salt'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///moodleDB'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+from sweater import models, routes
+
+db.create_all()
